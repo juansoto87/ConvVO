5fa36739ff57d73f934a2b423d15906559c5d9e8
juansoto87 <70484982+juansoto87@users.noreply.github.com>
2023-04-21 10:13:39 -0400

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -7,13 +7,18 @@ from joblib import Parallel, delayed
 import os
 import random
 
+'''
+This piece of code preprocesses the training and test images by creating pairs of resized images, 
+and returns a list with the names of the generated images.
+For the processing of the training images, a training subset and a validation subset are created.
+'''
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset_dir", type=str, required=False, help="where the dataset is stored")
 parser.add_argument("--resize", type=bool, default= False, help="If true remember to define width and height")
 parser.add_argument("--dest_dir", type=str, required=True, help="Where to dump the training data")
 parser.add_argument("--dataset", type=str, default= 'train', help="train or test")
 parser.add_argument("--test_dir", type=str, required=False, help="Where to dump the test data")
-parser.add_argument("--data_pose_dir", type=str, required=True, help="Where the pose dataset is stored")
 parser.add_argument("--type_prep", type=str, default= 'concat', help="Type of data preparation. concat or stack")
 parser.add_argument("--img_height", type=int, default=128, help="image height")
 parser.add_argument("--img_width", type=int, default=416, help="image width")
@@ -24,10 +29,12 @@ print(args.dataset_dir)
 parent_dir = os.getcwd()
 print(parent_dir)
 
+# List of training sequences
 train_seq =['00', '01', '02','03','04', '05', '06', '07', '08']
-#train_seq =['00']
+
+# List of test sequences
 test_seq = ['09','10']
-folder_test= ['test_folder']
+
 
 
 def folder_list(seq = None):
@@ -38,25 +45,21 @@ def folder_list(seq = None):
         for dir in seq:
             print(dir, len(os.listdir(os.path.join(args.dataset_dir,dir))))
 
+## Horizontal concatenation
 def get_concat_h(im1, im2):
     dst = Image.new('RGB', (im1.width + im2.width, im1.height))
     dst.paste(im1, (0, 0))
     dst.paste(im2, (im1.width, 0))
     return dst
 
-def extract_poses(file_name):
-    data_name = file_name+".npy"
-    print(data_name)
-
-
-
+## This function concat and save pairs of images, in addition it returns a list with the names of files saved.
 
 def concat(folder, destination):
     np_file = np.empty((0,6))
     for f in folder:
         image_list = []
         folder_path = os.path.join(args.dataset_dir, f)
-        data = np.load(os.path.join(args.data_pose_dir, (f+".npy")))
+
         for im in os.listdir(folder_path):
             image_list.append(im)
         image_list.sort()
@@ -68,11 +71,9 @@ def concat(folder, destination):
             if args.resize:
                 img1 = img1.resize((args.img_width, args.img_height))
                 img2 = img2.resize((args.img_width, args.img_height))
-            np_file = np.append(np_file, [[(data[i + 1, 0] - data[i, 0]), (data[i + 1, 1] - data[i, 1]),
-                          (data[i + 1, 2] - data[i, 2]), (data[i + 1, 3] - data[i, 3]),
-                          (data[i + 1, 4] - data[i, 4]), (data[i + 1, 5] - data[i, 5])]], axis=0)
+
             img_concat = get_concat_h(img1,img2).save(os.path.join(destination,(f + "_" + image_list[i])))
-    np.save(os.path.join(destination,'poses'), np_file)
+
     final_list=[]
     for im_name in os.listdir(destination):
         if '.png' in im_name:
@@ -96,11 +97,6 @@ def shuffle(list_p, array, random_seed =42):
     return list_p, array
 
 
-def chec_npy(main_folder,file):
-    data = np.load(os.path.join(main_folder,(file+".npy")))
-    for i in range(20):
-        print(data[i])
-    print(f"Longitud: {len(data)}")
 
 def train_valid_split(list_names, poses, valid_split=0.3):
     if len(list_names) == poses.shape[0]:
